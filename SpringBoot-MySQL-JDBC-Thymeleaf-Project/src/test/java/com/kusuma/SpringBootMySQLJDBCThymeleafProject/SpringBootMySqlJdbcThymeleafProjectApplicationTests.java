93bd1ab4694140e8c60dd02c6a93156f5365bb38
Kusumaraju27 <Kusumaraju27@gmail.com>
2021-07-07 11:56:14 +0530

    First commit

---
+++ SpringBoot-MySQL-JDBC-Thymeleaf-Project/src/test/java/com/kusuma/SpringBootMySQLJDBCThymeleafProject/SpringBootMySqlJdbcThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusuma.SpringBootMySQLJDBCThymeleafProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlJdbcThymeleafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
